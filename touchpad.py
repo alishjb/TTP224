28b987520af4e9836e3e7f34636e7fa74804f8d6
41i <48117947+alixpaydar@users.noreply.github.com>
2021-05-12 21:56:30 +0430

    Update touchpad.py
    Arduino Board Connected to Com8
    Working on windows with winsound

--- touchpad.py
+++ touchpad.py
@@ -1,23 +1,22 @@
-import pyfirmata
-import time
-board= pyfirmata.Arduino('COM5')
-it = pyfirmata.util.Iterator(board)
+import pyfirmata,winsound,time
+board = pyfirmata.Arduino('com8')
+it=pyfirmata.util.Iterator(board)
 it.start()
-touch1=board.get_pin('d:4:i')
-touch2=board.get_pin('d:5:i')
-touch3=board.get_pin('d:6:i')
-touch4=board.get_pin('d:7:i')
+touch1=board.get_pin('d:2:i')
+touch2=board.get_pin('d:3:i')
+touch3=board.get_pin('d:4:i')
+touch4=board.get_pin('d:5:i')
 while True:
-    valtouch1=touch1.read()
-    if valtouch1==True:
-        print('key 1 pressed')
-    valtouch2=touch2.read()
-    if valtouch2==True:
-        print('key 2 pressed')
-    valtouch3=touch3.read()
-    if valtouch3==True:
-        print('key 3 pressed')
-    valtouch4=touch4.read()
-    if valtouch4==True:
-        print('key 4 pressed')
-    time.sleep(1)
+    val1=touch1.read()
+    if val1 ==True:
+        winsound.PlaySound(r'Voice/1.wav',winsound.SND_FILENAME)
+    val2=touch2.read()
+    if val2 ==True:
+        winsound.PlaySound(r'Voice/2.wav',winsound.SND_FILENAME)
+    val3=touch3.read()
+    if val3 ==True:
+        winsound.PlaySound(r'Voice/3.wav',winsound.SND_FILENAME)
+    val4=touch4.read()
+    if val4 ==True:
+        winsound.PlaySound(r'Voice/4.wav',winsound.SND_FILENAME)
+    time.sleep(0.5)
